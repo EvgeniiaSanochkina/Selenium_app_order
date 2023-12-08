d26ab2568b1f18b53b29db2f2b9fd69d3b0a5bbc
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2023-12-08 12:03:05 +0700

    webdrivermanager is changed

--- src/test/java/TestOrder.java
+++ src/test/java/TestOrder.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
@@ -13,6 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TestOrder {
+
+
 
     private WebDriver driver;
 
